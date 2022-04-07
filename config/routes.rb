ca8526a5f20238f665d02fae9963885583a9c73a
E <effyverse@gmail.com>
2022-04-07 09:18:34 -0400

    add welcome resource for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root is needed for heroku
+  root 'welcome#index'
 end
