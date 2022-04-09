291e6efbc864669a80353458044d06023ba29aa6
E <effyverse@gmail.com>
2022-04-08 23:29:05 -0400

    add validations for alerts to reduce front end bugs

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,3 +1,7 @@
 class Alert < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, foreign_key: :user_id
+
+  validates :level, numericality: { only_integer: true }
+  validates_presence_of :level, :user_id
+
 end
