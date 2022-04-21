e9b23b6891f064925e090353efa9f83993ad6d02
E <effyverse@gmail.com>
2022-04-21 03:12:31 -0400

    fixed login bug of missing uid as required by devise by setting it up in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,17 @@ class User < ApplicationRecord
   has_many :thirds
 
   extend Devise::Models
+ 
+  # fixing weird unpermitted params error: https://stackoverflow.com/questions/30496770/devise-token-auth-cant-create-user-account-after-installing-devise-token-auth
+  before_validation :set_provider
+  before_validation :set_uid
 
+  def set_provider
+    self[:provider] = "email" if self[:provider].blank?
+  end
+
+  def set_uid
+    self[:uid] = self[:email] if self[:uid].blank? && self[:email].present?
+  end
 
 end
