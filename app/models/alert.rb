16dcb9552923cefa311b10d9d6c2a62805f6c6be
E <effyverse@gmail.com>
2022-05-08 02:17:33 -0400

    randomize timestamps for seeded models

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,7 +1,11 @@
 class Alert < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
-
   validates :level, numericality: { in: 1..9 }
   validates_presence_of :level, :user_id
+  # before_save :generate_timestamp
+
+  # def generate_timestamp
+  #   self.timestemp_field = DateTime.now
+  # end
 
 end
